6f9d596448d91d351d5111aec18b60f149fd9d3e
Łukasz Wieczorek <luke@soiree.tech>
2024-07-01 19:36:11 +0200

    Add client setup.

--- client/synchronizer/cli.py
+++ client/synchronizer/cli.py
@@ -1,4 +1,5 @@
 import dataclasses
+import getpass
 import json
 import os
 import pprint
@@ -38,7 +39,7 @@ class LocalSettings:
             all_settings = json.load(file_pointer)
 
         self.token = all_settings["token"]
-        self.station_id = all_settings["station_id"]
+        self.station_pk = all_settings["station_pk"]
 
         self.configs = []
 
@@ -49,19 +50,34 @@ class LocalSettings:
 class APICaller:
     API_ROOT = f"{PROTOCOL}{HOSTNAME}/api"
 
-    def __init__(self, settings: LocalSettings) -> None:
+    def __init__(self, settings: LocalSettings | None) -> None:
         self.settings = settings
 
     def headers(self) -> dict:
         dictionary = {"Authorization": f"Bearer {self.settings.token}"}
         return dictionary
 
-    def fetch_station(self, station_id: int) -> dict:
-        url = f"{self.API_ROOT}/stations/{station_id}"
+    def fetch_token(self, username: str, password: str) -> str:
+        url = f"{self.API_ROOT}/auth/token/"
+        data = {"username": username, "password": password}
+        response = requests.post(url, data=data)
+        authorization = response.json()
+        token = authorization["token"]
+        return token
+
+    def fetch_station(self, station_pk: int) -> dict:
+        url = f"{self.API_ROOT}/stations/{station_pk}"
         headers = self.headers()
         response = requests.get(url, headers=headers)
         station = response.json()
         return station
+
+    def fetch_stations(self) -> list:
+        url = f"{self.API_ROOT}/stations/"
+        headers = self.headers()
+        response = requests.get(url, headers=headers)
+        stations = response.json()
+        return stations
 
     def fetch_station_connections(self) -> dict:
         url = f"{self.API_ROOT}/station-connections/"
@@ -80,10 +96,10 @@ class APICaller:
             station_a = self.fetch_station(station_a_pk)
             station_b = self.fetch_station(station_b_pk)
 
-            if station_a_pk == self.settings.station_id:
+            if station_a_pk == self.settings.station_pk:
                 synchronized_path = station_a["path"]
                 push_to_station = station_b_pk
-            elif station_b_pk == self.settings.station_id:
+            elif station_b_pk == self.settings.station_pk:
                 synchronized_path = station_b["path"]
                 push_to_station = station_a
             else:
@@ -94,6 +110,16 @@ class APICaller:
             config = Config(synchronized_path, push_to_station)
             configs.append(config)
         self.settings.set_configs(configs)
+
+    def get_station_pk(self) -> int:
+        stations = self.fetch_stations()
+        print("Available stations:")
+        for station in stations:
+            print(f"{station["name"]}")
+        # TODO(lukasz.wieczorek): Implement.
+        station_pk_string = "1"
+        station_pk = int(station_pk_string)
+        return station_pk
 
 
 class EventHandler(events.FileSystemEventHandler):
@@ -164,8 +190,37 @@ def run() -> None:
     print("Exiting.")
 
 
+def save_settings(token: str, station_pk: int | None) -> None:
+    settings = {"token": token, "station_pk": station_pk}
+    with open("settings.json", "w") as file_pointer:
+        json.dump(settings, file_pointer, indent=4)
+        file_pointer.write("\n")
+
+
+def prompt(name: str, password: bool = False) -> str:
+    capitalized_name = name.capitalize()
+    if not password:
+        input_string = input(f"{capitalized_name}: ")
+    else:
+        input_string = getpass.getpass()
+    return input_string
+
+
 def setup():
-    pass
+    if os.path.exists("settings.json"):
+        print("Already setup.")
+        return
+
+    username, password = prompt("username"), prompt("password", password=True)
+
+    api_caller = APICaller(None)
+    token = api_caller.fetch_token(username, password)
+    save_settings(token, None)
+
+    settings = LocalSettings()
+    api_caller = APICaller(settings)
+    station_pk = api_caller.get_station_pk()
+    save_settings(token, station_pk)
 
 
 def print_help():
